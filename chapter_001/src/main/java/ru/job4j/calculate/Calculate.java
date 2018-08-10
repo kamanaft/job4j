8bf1e265c04ce243e8301f174131ee6307f79bbd
Alexey Zhukov <mailto:hadzage@gmail.com>
2018-08-10 19:55:05 +0200

    1.3. JavaDoc. Code convention [ #4087]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,8 +1,17 @@
-
 package ru.job4j.calculate;
 
-
+/**
+* Class Calculate, вывод строки в консоль.
+* @author Alexey Zhukov (hadzage@gmail.com)
+* @since 10.08.2018
+* @version 2
+*/
 public class Calculate {
+
+	/**
+ 	* Main.
+ 	* @param args - args.
+ 	*/
 
 	public static void main(String[] args) {
 		System.out.println("Hello, World!!!");
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Alexey Zhukov (mailto:hadzage@gmail.com)
+* @version 1
+* @since 10.08.2018
+*/
+package ru.job4j.calculate;
