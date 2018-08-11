74c20910aeb616284d62f5bd7922d4b650f3d90e
Alexey Zhukov <mailto:hadzage@gmail.com>
2018-08-11 18:24:33 +0200

    1.5. Checkstyle [ #4123]

---
+++ chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
@@ -0,0 +1,27 @@
+package ru.job4j.calculate;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+* Test.
+*
+* @author Alexey Zhukov (mailto:hadzage@gmail.com)
+* @version 01
+* @since 11.08.2018
+*/
+public class CalculateTest {
+/**
+* Test echo.
+*/
+@Test
+public void whenTakeNameThenTreeEchoPlusName() {
+    String input = "kamanaft";
+    String expect = "Echo, echo, echo : kamanaft"; 
+    Calculate calc = new Calculate();
+    String result = calc.echo(input);
+    assertThat(result, is(expect));
+}
+ 
+}
