78966b0710cdc200740eb19b09ba84b83b63b0cc
Alexey Zhukov <mailto:hadzage@gmail.com>
2018-08-12 16:12:59 +0200

    2.4. Использование Git в IDEA [ #31170]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,28 +1,31 @@
 package ru.job4j.calculate;
 
 /**
-* Class Calculate, вывод строки в консоль.
-* @author Alexey Zhukov (hadzage@gmail.com)
-* @since 10.08.2018
-* @version 2
-*/
+ * Class Calculate, вывод строки в консоль.
+ *
+ * @author Alexey Zhukov (hadzage@gmail.com)
+ * @version 2
+ * @since 10.08.2018
+ */
 public class Calculate {
 
-	/**
- 	* Main.
- 	* @param args - args.
- 	*/
+    /**
+     * Main.
+     *
+     * @param args - args.
+     */
 
-	public static void main(String[] args) {
-		System.out.println("Hello, World!!!");
-	}
+    public static void main(String[] args) {
+        System.out.println("Hello, World!!!");
+    }
 
-	/**
-	* Method echo.
-	* @param name Your name.
-	* @return Echo plus your name.
-	*/
-	public String echo(String name) {
-    	return "Echo, echo, echo : " + name;
-	}
+    /**
+     * Method echo.
+     *
+     * @param name Your name.
+     * @return Echo plus your name.
+     */
+    public String echo(String name) {
+        return "Echo, echo, echo : " + name;
+    }
 } 
--- chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
+++ chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
@@ -1,27 +1,28 @@
 package ru.job4j.calculate;
 
 import org.junit.Test;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 /**
-* Test.
-*
-* @author Alexey Zhukov (mailto:hadzage@gmail.com)
-* @version 01
-* @since 11.08.2018
-*/
+ * Test.
+ *
+ * @author Alexey Zhukov (mailto:hadzage@gmail.com)
+ * @version 01
+ * @since 11.08.2018
+ */
 public class CalculateTest {
-/**
-* Test echo.
-*/
-@Test
-public void whenTakeNameThenTreeEchoPlusName() {
-    String input = "kamanaft";
-    String expect = "Echo, echo, echo : kamanaft"; 
-    Calculate calc = new Calculate();
-    String result = calc.echo(input);
-    assertThat(result, is(expect));
-}
- 
+    /**
+     * Test echo.
+     */
+    @Test
+    public void whenTakeNameThenTreeEchoPlusName() {
+        String input = "kamanaft";
+        String expect = "Echo, echo, echo : kamanaft";
+        Calculate calc = new Calculate();
+        String result = calc.echo(input);
+        assertThat(result, is(expect));
+    }
+
 }
