65f8f0eb6b39df847bd1b1ca6f62088601c093ca
Alexey Zhukov <mailto:hadzage@gmail.com>
2018-08-22 11:55:55 +0200

    5.5. Рефакторинг кода. [ #33459]

--- chapter_001/src/test/java/ru/job4j/loop/PaintTest.java
+++ chapter_001/src/test/java/ru/job4j/loop/PaintTest.java
@@ -9,12 +9,55 @@ import static org.junit.Assert.*;
 
 /**
  * @author Alexey Zhukov (mailto:hadzage@gmail.com)
- * @version $01$
- * @since 18.08.2018
+ * @version $02$
+ * @since 22.08.2018
  */
 
 public class PaintTest {
+    /**
+     * Проверка вывода правой стороны треугольника.
+     */
     @Test
+    public void whenPyramid4Right() {
+        Paint paint = new Paint();
+        String rst = paint.rightTrl(4);
+        System.out.println(rst);
+        assertThat(rst,
+                is(
+                        new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                                .add("^   ")
+                                .add("^^  ")
+                                .add("^^^ ")
+                                .add("^^^^")
+                                .toString()
+                )
+        );
+    }
+
+    @Test
+    /**
+     * Проверка выводы левой стороны треугольника.
+     */
+    public void whenPyramid4Left() {
+        Paint paint = new Paint();
+        String rst = paint.leftTrl(4);
+        System.out.println(rst);
+        assertThat(rst,
+                is(
+                        new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                                .add("   ^")
+                                .add("  ^^")
+                                .add(" ^^^")
+                                .add("^^^^")
+                                .toString()
+                )
+        );
+    }
+
+    @Test
+    /**
+     * Проверка вывода обеих сторон треугольника.
+     */
     public void whenPyramid4() {
         Paint paint = new Paint();
         String rst = paint.pyramid(4);
