94c00056ba9f96be4b86ea88ce6e538971f6b4c2
Alexey Zhukov <mailto:hadzage@gmail.com>
2018-09-09 17:58:20 +0200

    Вопросы. [ #226]

--- chapter_001/src/main/java/ru/job4j/array/ThirdArray.java
+++ chapter_001/src/main/java/ru/job4j/array/ThirdArray.java
@@ -2,112 +2,45 @@ package ru.job4j.array;
 
 /**
  * @author Alexey Zhukov (mailto:hadzage@gmail.com)
- * @version $02$
- * @since 07.09.2018
+ * @version $03$
+ * @since 09.09.2018
  */
 
 
 public class ThirdArray {
-    int[] input1;
-    int[] input2;
-    int[] output = new int[5];
-
     public int[] together(int[] input1, int[] input2) {
-        int[] longAr;
-        int[] shortAr;
 
-        // Определяем самый длинный массив.
-        if (input1.length > input2.length) {
-            longAr = input1;
-            shortAr = input2;
+        int[] result = new int[input1.length + input2.length];
+        int index = 0;
+
+        int[] small = input1;
+        int[] big = input2;
+        if (input1.length < input2.length) {
+            small = input1;
+            big = input2;
         } else {
-            longAr = input2;
-            shortAr = input1;
+            small = input2;
+            big = input1;
         }
-        //Заполняем третий массив до окончания длины короткого массива.
-        int counter = output.length;
-        for (int in = 0; in != shortAr.length - 2; in = in + 2) {
-            if (input1[in] < input2[in]) {
-                output[in] = input1[in];
-                output[in + 1] = input2[in];
+        for (int i = 0; i < small.length; i++) {
+            if (small[i] < big[i]) {
+                result[index] = small[i];
+                index++;
+
+                result[index] = big[i];
+                index++;
             } else {
-                output[in] = input2[in];
-                output[in + 1] = input1[in];
+                result[index] = big[i];
+                index++;
+
+                result[index] = small[i];
+                index++;
             }
         }
-        //Заполняем оставшуюся часть массива.
-        for (int out = counter - shortAr.length; out != counter - 1; out++) {
-            output[out] = longAr[out - shortAr.length];
+        for (int j = 0; j < (big.length - small.length); j++) {
+            result[index] = big[small.length + j];
+            index++;
         }
-        return output;
-    }
-
-}
-
-
-
-/*    public static void main(String[] args) {
-        int[] arOne = new int[]{1, 3, 5};
-        int[] arTwo = new int[]{2, 4};
-        int[] arThree = new int[5];
-        int[] longAr;
-
-        if (arOne.length > arTwo.length) {
-            longAr = arOne;
-        } else {
-            longAr = arTwo;
-        }
-
-        for (int num: arThree) {
-            if (arOne[num] > arTwo[num] && num <= arOne.length && num <= arTwo.length) {
-                arThree[num] = arTwo[num];
-            } else {
-                arThree[num] = arOne[num];
-
-            if (num )
-        }
-
-
-    }
-}*/
-
-/*
-
-    public static void main(String[] args) {
-        int[] arOne = new int[]{1, 3, 5};
-        int[] arTwo = new int[]{2, 4};
-        int[] arThree = new int[5];
-        int[] longAr;
-        int min = Math.min(arOne.length, arTwo.length);
-        int max = Math.max(arOne.length, arTwo.length);
-
-        // Определяем самый длинный массив.
-        if (arOne.length > arTwo.length) {
-            longAr = arOne;
-        } else {
-            longAr = arTwo;
-        }
-        //Заполняем третий массив.
-        for (int index = 0; index != min - 1; index++) {
-            if (arOne[index] > arTwo[index]) {
-                arThree[index] = arTwo[index];
-                arThree[index + 1] = arOne[index];
-            } else {
-                arThree[index] = arOne[index];
-                arThree[index + 1] = arTwo[index];
-            }
-        }
-
-        for (int yandex = arThree.length - min; yandex != arThree.length - 1; yandex++) {
-            arThree[yandex] = longAr[yandex - 2];
-        }
-
-        for (int num : arThree) {
-            System.out.println(num);
-        }
+        return result;
     }
 }
-
-
-
-*/
--- chapter_001/src/test/java/ru/job4j/array/ThirdArrayTest.java
+++ chapter_001/src/test/java/ru/job4j/array/ThirdArrayTest.java
@@ -22,4 +22,25 @@ public class ThirdArrayTest {
         int[] expect = {1, 2, 3, 4, 5};
         assertThat(result, is(expect));
     }
+
+    @Test
+    public void whenTwoArraysAndOneIsEmptyGoToOneArrayWithOrder() {
+        ThirdArray thirdArray = new ThirdArray();
+        int[] input1 = new int[0];
+        int[] input2 = {2, 4, 8};
+        int[] result = thirdArray.together(input1, input2);
+        int[] expect = {2, 4, 8};
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenTwoArraysSameLengthGoToOneArrayWithOrder() {
+        ThirdArray thirdArray = new ThirdArray();
+        int[] input1 = {1, 3, 5};
+        int[] input2 = {2, 4, 6, 8};
+        int[] result = thirdArray.together(input1, input2);
+        int[] expect = {1, 2, 3, 4, 5, 6, 8};
+        assertThat(result, is(expect));
+    }
+
 }
