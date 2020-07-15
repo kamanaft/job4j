1917045780d0f8c95872fe3a0298a54116268563
kamanaft <kamanaft@pm.me>
2020-07-16 00:07:46 +0300

    Chess modul added

---
+++ chapter_002/src/main/java/ru/job4j/chess/Logic.java
@@ -0,0 +1,46 @@
+package ru.job4j.chess;
+
+import ru.job4j.chess.firuges.Cell;
+import ru.job4j.chess.firuges.Figure;
+
+import java.util.Arrays;
+
+/**
+ * //TODO add comments.
+ */
+public class Logic {
+    private final Figure[] figures = new Figure[32];
+    private int index = 0;
+
+    public void add(Figure figure) {
+        figures[index++] = figure;
+    }
+
+    public void move(Cell source, Cell dest)
+            throws FigureNotFoundException, ImpossibleMoveException, OccupiedCellException {
+        int index = findBy(source);
+        Cell[] steps = figures[index].way(source, dest);
+        if (!isFree(steps)) {
+            throw new OccupiedCellException();
+        }
+        figures[index] = figures[index].copy(dest);
+    }
+
+    private boolean isFree(Cell[] steps) {
+        return true;
+    }
+
+    public void clean() {
+        Arrays.fill(figures, null);
+        index = 0;
+    }
+
+    private int findBy(Cell cell) throws FigureNotFoundException {
+        for (int index = 0; index != figures.length; index++) {
+            if (figures[index] != null && figures[index].position().equals(cell)) {
+                return index;
+            }
+        }
+        throw new FigureNotFoundException();
+    }
+}
